12f5c0ab7dce581b73a58f147825d2644cec6b1e
Kemerson Maciel <131490497+kemersonMaciel@users.noreply.github.com>
2024-05-27 21:30:23 -0400

    alteração na Tela_Historico

--- src/Tela_Historicoped.java
+++ src/Tela_Historicoped.java
@@ -1,54 +1,83 @@
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.JComboBox;
-
-
 
 public class Tela_Historicoped extends JFrame implements ActionListener{
 
     JPanel painel = new JPanel();
 
-    JLabel lbtitulo = new JLabel("Últimos Pedidos");
-    JLabel lbcliente = new JLabel("Cliente");
-    JLabel lbdia = new JLabel("Dia");
-    JComboBox<String> cbopcao = new JComboBox<>();
-    JTextField txtcliente = new JTextField();
-
+    JLabel lbdia = new JLabel("DATA:");
+    JComboBox<String> cbData = new JComboBox<>();
+    JLabel txtcliente = new JLabel("Cliente:");
+    JLabel nomecliente = new JLabel("Kemerson Maciel");
+    JLabel txtpedido  = new JLabel("Pedido:");
+    JLabel quantpedido = new JLabel(" 2 ");
+    JLabel nomepedido = new JLabel("X-salada");
+    JLabel valorpedido = new JLabel("R$ 24,00");
 
     Tela_Historicoped(){
         setTitle("Histórico de Pedido");
-        setSize(340,450);
+        setSize(340, 450);
         setLocation(600, 150);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-        painel.setLayout(null);
+        painel.setLayout(null); 
 
-        lbtitulo.setBounds(90, 15,150,30);
-        lbcliente.setBounds(30, 50, 80, 30);
-        txtcliente.setBounds(30, 80, 150, 30);
-        lbdia.setBounds(30, 120, 280, 30);
-        cbopcao.setBounds(30,150,150,30);
+        lbdia.setBounds(30, 10, 80, 30);
+        cbData.setBounds(70, 15, 150, 20);
 
-        painel.add(lbtitulo);
-        painel.add(lbcliente);
-        painel.add(txtcliente);
+        txtcliente.setBounds(15, 45, 80, 30);
+        txtcliente.setFont(new Font("Serif",Font.BOLD,16));
+        nomecliente.setBounds(45, 65, 150, 30);
+        nomecliente.setFont(new Font("Monospaced", Font.ITALIC, 15));
+        txtpedido.setBounds(15, 95, 80, 30);
+        txtpedido.setFont(new Font("Serif", Font.BOLD, 16));
+        quantpedido.setBounds(40, 120, 150, 30);
+        quantpedido.setFont(new Font("Monospaced", Font.ITALIC, 15));
+        nomepedido.setBounds(70, 120, 100, 30);
+        nomepedido.setFont(new Font("Monospaced", Font.ITALIC, 15));
+        valorpedido.setBounds(200, 120, 150, 30);
+        valorpedido.setFont(new Font("Monospaced", Font.ITALIC, 15));
+
         painel.add(lbdia);
-        painel.add(cbopcao);
+        painel.add(cbData);
+        painel.add(txtcliente);
+        painel.add(nomecliente);
+        painel.add(txtpedido);
+        painel.add(quantpedido);
+        painel.add(nomepedido);
+        painel.add(valorpedido);
 
         getContentPane().add(painel);
 
-        cbopcao.addItem("segunda-feira");
-        cbopcao.addItem("terça-feira");
-        cbopcao.addItem("quarta-feira");
-        cbopcao.addItem("quinta-feira");
-        cbopcao.addItem("sexta-feira");
-        cbopcao.addItem("sábado");
-        cbopcao.addItem("domingo");
+        populateDateComboBox();
+
+    }
+
+    private void populateDateComboBox() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(Calendar.DAY_OF_YEAR, 1);
+        Date start = calendar.getTime();
+        
+        calendar.set(Calendar.DAY_OF_YEAR, calendar.getActualMaximum(Calendar.DAY_OF_YEAR));
+        Date end = calendar.getTime();
+        
+        Calendar current = new GregorianCalendar();
+        current.setTime(start);
+
+        while (current.getTime().before(end) || current.getTime().equals(end)) {
+            cbData.addItem(sdf.format(current.getTime()));
+            current.add(Calendar.DATE, 1);
+        }
     }
 
     public static void main(String[] args) {
@@ -58,8 +87,6 @@ public class Tela_Historicoped extends JFrame implements ActionListener{
     
     @Override
     public void actionPerformed(ActionEvent e) {
-        
-        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+        // Implementar a lógica de ação aqui, se necessário.
     }
-    
 }
--- src/Tela_Home.java
+++ src/Tela_Home.java
@@ -1,8 +1,9 @@
+import java.awt.Color;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,7 +16,7 @@ public class Tela_Home implements ActionListener {
     
     Produto pro;
 
-    JFrame tela = new JFrame("Lanche ADS");
+    JFrame tela = new JFrame("Kome-Kome Lanche");
     JPanel painel = new JPanel(new GridBagLayout());
 
     JMenuBar mb = new JMenuBar();
@@ -26,56 +27,65 @@ public class Tela_Home implements ActionListener {
     JMenuItem historicoMenuItem = new JMenuItem("Histórico de Pedido");
     JMenu buscarMenu = new JMenu("Buscar");
 
-        void criarTela(){
-            tela.setSize(500,500);
-            tela.setLocation(500,150);
-            tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    void criarTela() {
+        tela.setSize(500, 500);
+        tela.setLocation(500, 150);
+        tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        tela.getContentPane().setBackground(new Color(47, 51, 53)); // Configura a cor de fundo do JFrame
 
-            mb.add(cadastrarMenu);
-            mb.add(pedidoMenu);
-            mb.add(buscarMenu);
+        mb.add(cadastrarMenu);
+        mb.add(pedidoMenu);
+        mb.add(buscarMenu);
 
-            cadastrarMenu.add(cadastrarProduto);
-            pedidoMenu.add(novoPedidoItem);
-            pedidoMenu.add(historicoMenuItem);
+        cadastrarMenu.add(cadastrarProduto);
+        pedidoMenu.add(novoPedidoItem);
+        pedidoMenu.add(historicoMenuItem);
 
-            cadastrarProduto.addActionListener(this);
-            novoPedidoItem.addActionListener(this);
+        cadastrarProduto.addActionListener(this);
+        novoPedidoItem.addActionListener(this);
+        historicoMenuItem.addActionListener(this);
 
-            tela.setJMenuBar(mb);
+        tela.setJMenuBar(mb);
 
-            String imagePath = "src/source/Lanche1.png";
-            ImageIcon icon = new ImageIcon(imagePath);
-            JLabel imgLabel = new JLabel(icon);
-            painel.add(imgLabel);
+        String imagePath = "src/source/Lanche.png";
+        ImageIcon icon = new ImageIcon(imagePath);
+        JLabel imgLabel = new JLabel(icon);
 
-            tela.add(painel);
-            tela.setVisible(true);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.CENTER;
+        painel.add(imgLabel, gbc);
 
-            cadastrarMenu.setFont(new Font("Segoe UI", Font.BOLD, 14));
-            cadastrarProduto.setFont(new Font("Segoe UI", Font.BOLD, 14));
-            pedidoMenu.setFont(new Font("Segoe UI", Font.BOLD, 14));
-            novoPedidoItem.setFont(new Font("Segoe UI", Font.BOLD, 14));
-            historicoMenuItem.setFont(new Font("Segoe UI", Font.BOLD, 14));
-            buscarMenu.setFont(new Font("Segoe UI", Font.BOLD, 14));
-        }
-        
-        public static void main(String[] args) {
+        painel.setBackground(new Color(47, 51, 53)); // Configura a cor de fundo do painel
+        tela.add(painel);
+        tela.setVisible(true);
+
+        // Configura as fontes dos menus e itens de menu
+        cadastrarMenu.setFont(new Font("Segoe UI", Font.BOLD, 14));
+        cadastrarProduto.setFont(new Font("Segoe UI", Font.BOLD, 14));
+        pedidoMenu.setFont(new Font("Segoe UI", Font.BOLD, 14));
+        novoPedidoItem.setFont(new Font("Segoe UI", Font.BOLD, 14));
+        historicoMenuItem.setFont(new Font("Segoe UI", Font.BOLD, 14));
+        buscarMenu.setFont(new Font("Segoe UI", Font.BOLD, 14));
+    }
+
+    public static void main(String[] args) {
         Tela_Home tela1 = new Tela_Home();
         tela1.criarTela();
-        }
+    }
 
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if (e.getSource() == cadastrarProduto){
-                Tela_Cadasprod telacadas = new Tela_Cadasprod();
-                telacadas.setVisible(true);
-            } else if (e.getSource() == novoPedidoItem){
-                Tela_Novoped novopedido = new Tela_Novoped();
-                novopedido.setVisible(true);
-            } else if (e.getSource()== historicoMenuItem){
-                Tela_Historicoped historicoped = new Tela_Historicoped();
-                historicoped.setVisible(true);
-            }
-        }    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == cadastrarProduto) {
+            Tela_Cadasprod telacadas = new Tela_Cadasprod();
+            telacadas.setVisible(true);
+        } else if (e.getSource() == novoPedidoItem) {
+            Tela_Novoped novopedido = new Tela_Novoped();
+            novopedido.setVisible(true);
+        } else if (e.getSource() == historicoMenuItem) {
+            Tela_Historicoped historicoped = new Tela_Historicoped();
+            historicoped.setVisible(true);
+        }
+    }
 }
